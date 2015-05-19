ad2fff7c86eef0a3f29deb4f481ffa040b960585
Rey Thompson <rthompson1@gmail.com>
2015-05-19 14:58:48 -0500

    second commit

--- coins.js
+++ coins.js
@@ -7,28 +7,33 @@
 //
 // The available coins are 25¢, 10¢, 5¢, 1¢
 
-function coins(sum){
-	var coinsReturned = []
-	var tender = [25, 10, 5, 1]
-	tender.reduce(function (customer, value) {
-		if (customer >= value) {
-			var times = Math.floor(customer/value)
-			for (i = 0; i < times; i++) [
-				coinsReturned.push(value)
-				customer -= value
-			]
+function coins(sum) {
+    var coinsReturned = []
+    var tender = [25, 10, 05, 01]
+    tender.reduce(function(owed, value) {
+        var times = 0
+        if (owed >= value) {
+            times = Math.floor(owed / value)
+            for (i = 0; i < times; i++) {
+                coinsReturned.push(value)
+            }
+        }
+        return owed -= (times * value)
 
-		}
-	}, sum)
-	return coinsReturned
+    }, sum)
+    return coinsReturned
 }
+
+
+
+
 // tests
 // ---
 var result = coins(11)
-// --> 10, 1
-console.assert( result instanceof Array )
-console.assert( result[0] === 10 && result[1] === 1 )
+    // --> 10, 1
+console.assert(result instanceof Array)
+console.assert(result[0] === 10 && result[1] === 1)
 
 result = coins(39)
-// --> 25, 10, 1, 1, 1, 1
-console.assert( result[0] === 25 && result[1] === 10 && result[2] === 1 && result[3] === 1 && result[4] === 1 && result[5] === 1)
+    // --> 25, 10, 1, 1, 1, 1
+console.assert(result[0] === 25 && result[1] === 10 && result[2] === 1 && result[3] === 1 && result[4] === 1 && result[5] === 1)
